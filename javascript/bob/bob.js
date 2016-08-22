01306c27ecf4b1d4089d2794bd030765b61f73b6
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-08-22 16:10:58 -0600

    Completed Bob in javascript

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,15 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+if (input === input.toUpperCase() && /[a-z]/i.test(input)) {
+  return 'Whoa, chill out!'
+} else if (input.slice(-1) === '?') {
+  return 'Sure.'
+} else if (/^\s*$/.test(input)) {
+  return 'Fine. Be that way!'
+} else {
+  return 'Whatever.'
+}
 };
 
 module.exports = Bob;
