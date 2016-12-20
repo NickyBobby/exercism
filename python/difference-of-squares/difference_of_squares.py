78303fd979c4dc83ad0f30fdc6ecb56e62a4e5da
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-12-20 18:10:42 -0500

    Starts Difference of Squares in Python

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,13 @@
+def square_of_sum(num):
+    sum = 1
+    count = 1
+    while count < num:
+        count += 1
+        sum = sum + count
+    return sum**2
+
+def difference():
+    return
+
+def sum_of_squares():
+    return
